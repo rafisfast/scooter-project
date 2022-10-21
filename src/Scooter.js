54731636a14dd9435aac56c725bd2b991105e09c
Rafay Tahir <rafayatahir@fb.com>
2022-10-21 12:28:07 +0100

    classes programmed

--- src/Scooter.js
+++ src/Scooter.js
@@ -1,5 +1,42 @@
 class Scooter{
-  // scooter code here
+  constructor(station, user) {
+    this.station = station;
+    this.user = user;
+    this.serial = Math.floor(Math.random()*1000);
+    this.charge = Math.floor(Math.random()*100);
+    this.isBroken = false;
+    this.docked = true;
+  }
+
+  rent =() => {
+    if (!this.isBroken && this.charge > 20 && !this.docked) {
+      console.log("Enjoy the ride!")
+    } else if (this.charge <= 20) {
+      console.log("Scooter low on battery, please charge.")
+    } else {
+      console.log("Scooter is broken, please send a repair request.")
+    }
+  }
+
+  dock = (station) => {
+    if (!station) { console.log("Docking station required!"); return }
+    this.station = station;
+    this.docked = true;
+    this.user = "";
+  }
+
+  recharge = async () => {
+    await new Promise(resolve => setTimeout(resolve, 2000));
+    console.log("scooter has recharged");
+    this.charge = 100;
+  }
+
+  requestRepair = async () => {
+    await new Promise(resolve => setTimeout(resolve, 2000));
+    console.log("repair has been completed");
+    this.isBroken = false;
+  }
+
 }
 
 
--- src/ScooterApp.js
+++ src/ScooterApp.js
@@ -3,6 +3,65 @@ const Scooter = require('./Scooter')
 
 class ScooterApp {
   // ScooterApp code here
+   static #stations = {
+    "Manhattan":[],
+    "Brooklyn":[],
+    "Queens":[],
+    "Bronx":[],
+    "StatenIsland":[]
+  }
+
+  static #registeredUsers = {}
+
+  register = (user) => {
+
+    if (this.#registeredUsers[user]) {
+      console.log("already registered!")
+      return
+    }
+
+    if (user.age < 17) {
+      console.log("too young to register!")
+      return
+    }
+
+    this.#registeredUsers[user] = {
+      "password" : user.password,
+      "age" : user.age,
+      "loggedIn": false,
+      "accountChange": 0
+    }
+
+    console.log("user has been registered")
+
+  }
+
+  logIn = (username, password) => {
+    if (this.#registeredUsers[username] && this.#registeredUsers[username].password === password) {
+      this.#registeredUsers[username].loggedIn = true
+      console.log("username has logged in")
+    } else {
+      console.log("Username or password is incorrect.")
+    }
+  }
+
+  addScooter = (location, scooter) => {
+    scooter.station = location;
+    this.#stations.push(scooter);
+  }
+
+  RemoveScooter = (scooterToRemove) => {
+    if (scooterToRemove >= 0 && scooterToRemove <= 1000) {
+      this.#stations.map((station, stationIndex) => {
+        station.map((scooter, scooterIndex) => {
+          if (scooter.serial === scooterToRemove) {
+            delete this.#stations[stationIndex][scooterIndex];
+          }
+        })
+      })
+    }
+  }
+
 }
 
 module.exports = ScooterApp
