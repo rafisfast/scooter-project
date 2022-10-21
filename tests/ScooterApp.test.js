b20fd753a03cac067a32b576bde3da09af0fb01f
Rafay Tahir <rafayatahir@fb.com>
2022-10-21 11:52:43 +0100

    classes diagram static + private

--- tests/ScooterApp.test.js
+++ tests/ScooterApp.test.js
@@ -2,7 +2,15 @@ const Scooter = require('../src/Scooter')
 const User = require('../src/User')
 const ScooterApp = require('../src/ScooterApp')
 
+const scooter = new ScooterApp()
+
 // ScooterApp tests here
+
+describe("scooter object", ()=> {
+    test("init properly", ()=> {
+        expect()
+    })
+})
 
 // register user
 
